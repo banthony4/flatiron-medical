0bca8971c3bf9065f4158a66d1b0575bde2e80d5
Ben <banthony446@gmail.com>
2022-06-22 17:23:09 -0400

    unique keys for carousel dots

--- client/src/Components/Home/carousel/sliderImage.js
+++ client/src/Components/Home/carousel/sliderImage.js
@@ -1,18 +1,22 @@
 
 const sliderImages = [
   {
+    id: 1,
     title: "First Slide",
     url: "https://ak.picdn.net/shutterstock/videos/21924376/thumb/7.jpg",
   },
   {
+    id: 2,
     title: "Second Slide",
     url: "https://medesol.com/wp-content/uploads/2022/04/Singapore-Nurses-Images.jpg",
   },
   {
+    id: 3,
     title: "Third Slide",
     url:"https://www.amc.edu/aboutus/images/about_us_static_2.jpg",
   },
   {
+    id: 4,
     title: "Fourth Slide",
     url:"https://hhinternet.blob.core.windows.net/uploads/2018/10/nursing-excellence-wards-2018-feature.jpg",
   }
